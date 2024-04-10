32bc239cd831662379820645a65233b69e51e01b
Nikita Patel <niki75757@gmail.com>
2024-04-10 16:05:54 +0530

    first commit

---
+++ play.js
@@ -0,0 +1,41 @@
+// var name = 'Heming'
+// var age = '28'
+// var hashobbie = 'Football'
+
+// function summarizeUser(userName,userAge,hasHobbie){
+
+//     return(
+//         'Name is' + 
+//         userName + 
+//         ',Age is' +
+//         userAge +
+//         'and user has hobbies : ' +
+//         hasHobbie
+//     );
+
+// }
+// console.log(summarizeUser(name, age, hashobbie));
+
+
+// const person = {
+//     name : 'Bhumit',
+//     age : '25',
+//     greet(){
+//         console.log('My name is: ' + this.name);
+//         console.log('My age is: ' + this.age);
+//     }
+// }
+
+// person.greet();
+
+
+const hobbies = ['reading','codeing'];
+// for(let hobby of hobbies){
+//     console.log(hobby);
+// }
+
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby))
+// console.log(hobbies)
+
+hobbies.push('cricket');
+console.log(hobbies);
