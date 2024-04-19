9384bb0c99490264ede9fb10f99ea7ed3a73a4df
Bhumit <bhumitpatel58@gmail.com>
2024-04-19 16:32:26 +0530

    05/017

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,13 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
+const rootDir =  require('../util/path')
+    // router.get( '/add_product', (req,res,next) => {
+    //     res.send('<body><form action="/admin/add_product" method="POST"><input type="text" name ="title"><button type="submit">Send</button></form></bod>')
+    // });
 
     router.get( '/add_product', (req,res,next) => {
-        res.send('<body><form action="/admin/add_product" method="POST"><input type="text" name ="title"><button type="submit">Send</button></form></bod>')
+        res.sendFile(path.join(rootDir, 'views', 'add_product.html'));
     });
     router.post( '/add_product', (req,res,next) => {
         console.log(req.body);
