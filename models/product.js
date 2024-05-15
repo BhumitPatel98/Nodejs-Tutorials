5aec13718fa5be3e0fcb947024620967b9729ac3
Bhumit <bhumitpatel58@gmail.com>
2024-05-15 17:26:48 +0530

    08/01

--- models/product.js
+++ models/product.js
@@ -1,33 +1,38 @@
-const path = require("../util/path");
+const fs = require('fs');
+const path = require('path');
 
-const products = [];
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
 
+const getProductsFromFile = cb =>{
+    fs.readFile(p, (err, filecontent) => {
+        if (err) {
+            cb ([]);
+        }     
+        cb(JSON.parse(filecontent))    
+    });
+};
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
 
     save(){
-        const p = path.join(
-                  path.dirname(process.mainModule.filename),
-                 'data',
-                 'products.json'
-        );
-        fs.readFile(p,(err,filecontent) => {
-                let products = [];
-                if(!err) {
-                    products = JSON.parse(filecontent);
-                }
-                products.push(this);
-                fs.writeFile(p, JSON.stringify(products), (err) => {
+        getProductsFromFile(products => {
+            products.push(this);
+                fs.writeFile(p, JSON.stringify(products), err => {
                     console.log(err);
                 });
         });
+        fs.readFile(p, (err, filecontent) => {});
 
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+        getProductsFromFile(cb);
     }
-}
+};
 
