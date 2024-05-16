7c639b1d7ac1f88db4915500768e7b52ae84d904
Bhumit <bhumitpatel58@gmail.com>
2024-05-16 16:24:28 +0530

    08/04

--- routes/shop.js
+++ routes/shop.js
@@ -5,5 +5,8 @@ const productsController = require('../controllers/products');
 const router = express.Router();
 
 router.get('/', productsController.getProducts);
+router.get('/products');
+router.get('/cart');
+router.get('/checkout');
 
 module.exports = router;
